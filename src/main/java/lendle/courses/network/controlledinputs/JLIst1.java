19bf46e1def9fb60903a1d0b3f87631275164663
user <user@042>
2018-10-16 17:01:25 +0800

    BorderSample

--- src/main/java/lendle/courses/network/controlledinputs/JLIst1.java
+++ src/main/java/lendle/courses/network/controlledinputs/JLIst1.java
@@ -6,10 +6,15 @@
 package lendle.courses.network.controlledinputs;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Vector;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
 import javax.swing.WindowConstants;
 
 /**
@@ -24,14 +29,38 @@ public class JLIst1 {
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
         JFrame frame = new JFrame();
-        frame.setSize(300, 300);
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         //create JList and add it to a JScrollPane
         JScrollPane scrollPane=new JScrollPane();
+        Vector v=new Vector();
+        v.add("0");
+        v.add(6);
+        v.add(new Object());
+        JList list=new JList(v);
+        list.setCellRenderer(new ListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                JLabel bt=new JLabel();
+                if(value instanceof String){
+                    bt.setText(""+value);
+                }else if(value instanceof Integer){
+                    bt.setText(""+value);
+                }else{
+                    bt.setText(""+value.hashCode());
+                }
+                if(isSelected){
+                    bt.setOpaque(true);
+                    bt.setBackground(Color.red);
+                }
+                return bt;
+            }
+        });
+        
         //////////////////////////////////////////
         frame.add(scrollPane);
-        
+        scrollPane.getViewport().add(list);
         frame.setVisible(true);
     }
     
